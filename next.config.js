876296aedc63145955b1c7ce8b2ff19f1ee45d5f
terinkavermi-sketch <terinka.vermi@gmail.com>
2026-01-20 21:59:50 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,55 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+    browserDebugInfoInTerminal: true,
+  },
+  reactStrictMode: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: { unoptimized: true },
+  devIndicators: false,
+  allowedDevOrigins: [
+    "*.macaly.dev",
+    "*.macaly.app",
+    "*.macaly-app.com",
+    "*.macaly-user-data.dev",
+  ],
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        condition: {
+          all: [{ not: "foreign" }, "development"],
+        },
+        loaders: [
+          {
+            loader: "macaly-tagger",
+            options: {
+              disableSourceMaps: true,
+            },
+          },
+        ],
+        as: "*",
+      },
+    },
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (dev) {
+      config.module.rules.unshift({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "macaly-tagger",
+          },
+        ],
+        enforce: "pre",
+      });
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
